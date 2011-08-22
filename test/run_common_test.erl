61034361277a2ca4057a346e4ce6b0c048a40935
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2011-08-22 14:14:31 +0200

    Test framework added

---
+++ test/run_common_test.erl
@@ -0,0 +1,31 @@
+-module(run_common_test).
+-export([ct/0]).
+
+-define(CT_DIR, filename:join([".", "suites"])).
+-define(CT_REPORTS, filename:join([".", "reports"])).
+-define(CT_CONFIG, "test.config").
+
+ct() ->
+    ct:run_test([
+        {config, [?CT_CONFIG]},
+        {dir, ?CT_DIR},
+        {logdir, ?CT_REPORTS}
+
+        %% add suites below like that:
+
+        %% the suite
+        %, {suite, "privacy_SUITE"}
+        
+        %% suite's group no. 1
+        %, {group, [management]}
+        %% group no. 1 tests
+        %, {testcase, get_all_lists_with_active}
+        %, {testcase, get_all_lists_with_default}
+
+        %% suite's group no. 2
+        %, {group, [blocking]}
+        %% group no. 2 tests
+        %, {testcase, block_jid_message}
+        %, {testcase, block_jid_all}
+    ]),
+    init:stop(0).
---
+++ test/suites/offline_SUITE.erl
@@ -0,0 +1,57 @@
+-module(offline_SUITE).
+-compile(export_all).
+
+-include_lib("escalus/include/escalus.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% Suite configuration
+%%--------------------------------------------------------------------
+
+all() ->
+    [{group, the_group}].
+
+groups() ->
+    [{the_group, [sequence], [testcase1
+                              ]}].
+
+suite() ->
+    escalus:suite().
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+init_per_suite(Config) ->
+    escalus:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    escalus:end_per_suite(Config).
+
+init_per_group(_GroupName, Config) ->
+    escalus:create_users(Config).
+
+end_per_group(_GroupName, Config) ->
+    escalus:delete_users(Config).
+
+init_per_testcase(CaseName, Config) ->
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
+
+%%--------------------------------------------------------------------
+%% Tests
+%%--------------------------------------------------------------------
+
+testcase1(Config) ->
+    escalus:story(Config, [1], fun(_Mary) ->
+
+        ok
+
+        end).
+
+%%-----------------------------------------------------------------
+%% Helpers
+%%-----------------------------------------------------------------
+
