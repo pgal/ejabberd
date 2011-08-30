55b15a294196d31df48f61a27e08986fdfce014d
Radosław Szymczyszyn <lavrin@gmail.com>
2011-08-30 23:27:39 +0200

    mod_offline_riak cleanup
    Removed unused code (mnesia leftovers). Rearranged some functions.

--- apps/ejabberd/src/mod_offline_riak.erl
+++ apps/ejabberd/src/mod_offline_riak.erl
@@ -11,16 +11,6 @@
 -type user() :: {string_type(), string_type()}.
 -type time() :: {integer(), integer(), integer()}.
 
-get_messages(US) ->
-    case ejabberd_riak:get(?OFFLINE_BUCKET, US) of
-        {ok, Msgs} ->
-            {ok, Msgs};
-        {error, notfound} ->
-            {ok, []};
-        Error ->
-            Error
-    end.
-
 create_table() ->
     ok.
 
@@ -64,152 +54,65 @@ load_offline_messages(US, Opts) ->
 
 -spec remove_expired_messages(time()) -> ok | {error, any()}.
 remove_expired_messages(TimeStamp) ->
-    F = fun() ->
-        mnesia:write_lock_table(offline_msg),
-        mnesia:foldl(
-            fun(Rec, _Acc) ->
-                case Rec#offline_msg.expire of
-                    never ->
-                        ok;
-                    TS ->
-                        if
-                            TS < TimeStamp ->
-                                mnesia:delete_object(Rec);
-                            true ->
-                                ok
-                        end
-                end
-            end, ok, offline_msg)
-    end,
-    transaction_no_result(F).
+    {error, not_implemented_yet}.
+    %mnesia:foldl(
+        %fun(Rec, _Acc) ->
+            %case Rec#offline_msg.expire of
+                %never ->
+                    %ok;
+                %TS ->
+                    %if
+                        %TS < TimeStamp ->
+                            %mnesia:delete_object(Rec);
+                        %true ->
+                            %ok
+                    %end
+            %end
+        %end, ok, offline_msg)
 
 -spec remove_old_messages(time()) -> ok | {error, any()}.
 remove_old_messages(TimeStamp) ->
-    F = fun() ->
-        mnesia:write_lock_table(offline_msg),
-        mnesia:foldl(
-            fun(#offline_msg{timestamp = TS} = Rec, _Acc)
-                when TS < TimeStamp ->
-                    mnesia:delete_object(Rec);
-                (_Rec, _Acc) -> ok
-            end, ok, offline_msg)
-    end,
-    transaction_no_result(F).
+    {error, not_implemented_yet}.
+    %F = fun() ->
+        %mnesia:write_lock_table(offline_msg),
+        %mnesia:foldl(
+            %fun(#offline_msg{timestamp = TS} = Rec, _Acc)
+                %when TS < TimeStamp ->
+                    %mnesia:delete_object(Rec);
+                %(_Rec, _Acc) -> ok
+            %end, ok, offline_msg)
+    %end,
+    %transaction_no_result(F).
 
 -spec remove_user(user()) -> ok | {error, any()}.
 remove_user(US) ->
-    F = fun() ->
-        mnesia:delete({offline_msg, US})
-    end,
-    transaction_no_result(F).
+    ejabberd_riak:delete(?OFFLINE_BUCKET, US).
 
 %% Supported Opts:
 %% - write_lock - the whole table is write locked at the start of the
 %%   transaction
 -spec delete_offline_messages(list(), list()) -> ok | {error, any()}.
 delete_offline_messages(Messages, Opts) ->
-    F = fun() ->
-        case proplists:get_value(write_lock, Opts, false) of
-            true ->
-                mnesia:write_lock_table(offline_msg);
-            false ->
-                ok
-        end,
-        lists:foreach(fun(Msg) -> mnesia:delete_object(Msg) end, Messages)
-    end,
-    transaction_no_result(F).
+    {error, not_implemented_yet}.
+    %F = fun() ->
+        %case proplists:get_value(write_lock, Opts, false) of
+            %true ->
+                %mnesia:write_lock_table(offline_msg);
+            %false ->
+                %ok
+        %end,
+        %lists:foreach(fun(Msg) -> mnesia:delete_object(Msg) end, Messages)
+    %end,
+    %transaction_no_result(F).
 
 %%% Helpers
 
-transaction_no_result(F) ->
-    case mnesia:transaction(F) of
-        {atomic, _} ->
-            ok;
+get_messages(US) ->
+    case ejabberd_riak:get(?OFFLINE_BUCKET, US) of
+        {ok, Msgs} ->
+            {ok, Msgs};
+        {error, notfound} ->
+            {ok, []};
         Error ->
-            {error, Error}
-    end.
-
-update_table() ->
-    Fields = record_info(fields, offline_msg),
-    case mnesia:table_info(offline_msg, attributes) of
-        Fields ->
-            ok;
-        [user, timestamp, expire, from, to, packet] ->
-            ?INFO_MSG("Converting offline_msg table from "
-                "{user, timestamp, expire, from, to, packet} format", []),
-            Host = ?MYNAME,
-            {atomic, ok} = mnesia:create_table(
-                mod_offline_tmp_table,
-                [{disc_only_copies, [node()]},
-                    {type, bag},
-                    {local_content, true},
-                    {record_name, offline_msg},
-                    {attributes, record_info(fields, offline_msg)}]),
-            mnesia:transform_table(offline_msg, ignore, Fields),
-            F1 = fun() ->
-                    mnesia:write_lock_table(mod_offline_tmp_table),
-                    mnesia:foldl(
-                        fun(#offline_msg{us = U} = R, _) ->
-                                mnesia:dirty_write(
-                                    mod_offline_tmp_table,
-                                    R#offline_msg{us = {U, Host}})
-                        end, ok, offline_msg)
-            end,
-            mnesia:transaction(F1),
-            mnesia:clear_table(offline_msg),
-            F2 = fun() ->
-                    mnesia:write_lock_table(offline_msg),
-                    mnesia:foldl(
-                        fun(R, _) ->
-                                mnesia:dirty_write(R)
-                        end, ok, mod_offline_tmp_table)
-            end,
-            mnesia:transaction(F2),
-            mnesia:delete_table(mod_offline_tmp_table);
-        [user, timestamp, from, to, packet] ->
-            ?INFO_MSG("Converting offline_msg table from "
-                "{user, timestamp, from, to, packet} format", []),
-            Host = ?MYNAME,
-            {atomic, ok} = mnesia:create_table(
-                mod_offline_tmp_table,
-                [{disc_only_copies, [node()]},
-                    {type, bag},
-                    {local_content, true},
-                    {record_name, offline_msg},
-                    {attributes, record_info(fields, offline_msg)}]),
-            mnesia:transform_table(
-                offline_msg,
-                fun({_, U, TS, F, T, P}) ->
-                        {xmlelement, _Name, _Attrs, Els} = P,
-                        Expire = mod_offline:find_x_expire(TS, Els),
-                        #offline_msg{us = U,
-                            timestamp = TS,
-                            expire = Expire,
-                            from = F,
-                            to = T,
-                            packet = P}
-                end, Fields),
-            F1 = fun() ->
-                    mnesia:write_lock_table(mod_offline_tmp_table),
-                    mnesia:foldl(
-                        fun(#offline_msg{us = U} = R, _) ->
-                                mnesia:dirty_write(
-                                    mod_offline_tmp_table,
-                                    R#offline_msg{us = {U, Host}})
-                        end, ok, offline_msg)
-            end,
-            mnesia:transaction(F1),
-            mnesia:clear_table(offline_msg),
-            F2 = fun() ->
-                    mnesia:write_lock_table(offline_msg),
-                    mnesia:foldl(
-                        fun(R, _) ->
-                                mnesia:dirty_write(R)
-                        end, ok, mod_offline_tmp_table)
-            end,
-            mnesia:transaction(F2),
-            mnesia:delete_table(mod_offline_tmp_table);
-        _ ->
-            ?INFO_MSG("Recreating offline_msg table", []),
-            mnesia:transform_table(offline_msg, ignore, Fields)
+            Error
     end.
